753f022a4a4cd42e18b40967b70bb80d057fdba0
Surya <suryapanneer04@gmail.com>
2023-12-29 19:59:42 +0530

    java Control Statement

---
+++ Control_Statement/ForLoop.java
@@ -0,0 +1,10 @@
+package Control_Statement;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
