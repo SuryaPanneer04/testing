3fe53ade5f9d8c81ec4933143fe452e5dad2135f
Surya <suryapanneer04@gmail.com>
2024-01-04 19:23:18 +0530

    java Control Statement

---
+++ Arrays_Examples/demo.java
@@ -0,0 +1,11 @@
+package Arrays_Examples;
+public class demo {
+    public static void main(String[] args) {
+        int[] myarray ={10, 20, 30, 40};
+        int FirstNumber=myarray[0]; //0 position store the first number 10
+        System.out.println(FirstNumber);
+        myarray[1]=100;// change the 1 position value
+        int demo=myarray[1];
+        System.out.println(demo);
+    }
+}
