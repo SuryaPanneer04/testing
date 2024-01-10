cc2c7616535730874fb40223265e1a72c1acfc12
Surya <suryapanneer04@gmail.com>
2024-01-10 21:11:31 +0530

    Example for inheritance

---
+++ OopsExamplePrograms/Child.java
@@ -0,0 +1,17 @@
+package OopsExamplePrograms;
+
+public class Child extends Parent {
+   void display(){
+       System.out.println("Name is :"+name);
+       System.out.println("RollNo is :"+rollno);
+       System.out.println("Address is:"+address);
+
+   }
+    public static void main(String[] args) {
+        Child ob=new Child();
+        ob.getStudent();
+        ob.getAddress();
+        ob.display();
+
+    }
+}
